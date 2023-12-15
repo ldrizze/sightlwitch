a61c9d15d14f0058127a0415f8ca98b4575851f8
Teco <matheus.acrivellari@gmail.com>
2023-12-15 02:42:46 -0300

    feat(level design): add level 9

--- levels-scripts/level_9_script.gd
+++ levels-scripts/level_9_script.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+
 func _ready():
 	LightSwitchController.reload()
 	LightSwitchController.blue()
 
+
 func _on_next_scene_timer_timeout():
 	next_level()
-
+	
 
 func next_level():
-	get_tree().change_scene_to_file("res://levels/level_9.tscn")
+	get_tree().change_scene_to_file("res://levels/level_10.tscn")
